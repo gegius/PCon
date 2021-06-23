736dc3880f52ed58d3961b2149a90efab465d3ef
HELLoWorlD01100 <danil7953@mail.ru>
2021-06-23 08:32:05 +0500

    Add new error

---
+++ PCon/Infrastructure/LibVlcHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace PCon.Infrastructure
+{
+    public class LibVlcHelper
+    {
+        public DirectoryInfo GetVlcPlayerPath()
+        {
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            if (!Directory.Exists(Path.Combine(currentDirectory, "libvlc")))
+                ZipFile.ExtractToDirectory(Path.Combine(currentDirectory, "libvlc.zip"), currentDirectory);
+            return new DirectoryInfo(Path.Combine(currentDirectory, "libvlc",
+                Environment.Is64BitOperatingSystem ? "win-x64" : "win-x86"));
+        }
+    }
+}
--- PCon/View/ErrorHandler.cs
+++ PCon/View/ErrorHandler.cs
@@ -6,13 +6,18 @@ namespace PCon.View
     {
         public static void ThrowErrorConnection()
         {
-            MessageBox.Show("Упс... Что-то пошло не так", "PCon", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show("Упс... Что-то пошло не так.\n\nПроверьте соединение с интернетом.", "PCon", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         
         public static void ThrowErrorNotSelectedProcess()
         {
             MessageBox.Show("Выбери, пожалуйста, процесс...", "PCon", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void ThrowErrorNoVlcPlayer()
+        {
+            MessageBox.Show("Vlc player не найден в базовой комплектации.\n\nПопробуйте переустановить программу.", "PCon", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
